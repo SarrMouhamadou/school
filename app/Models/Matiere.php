0e3d51364e9c0dac7a83d7878d890cceff868e29
Saluisito9980 <saluisitosene@gmail.com>
2025-07-22 15:35:41 +0000

    Création des models enseignants et matieres

---
+++ app/Models/Matiere.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Matiere extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nom', 'coefficient'];
+
+    public function enseignants()
+    {
+        return $this->belongsToMany(Enseignant::class)->withPivot('classe_id');
+    }
+}
